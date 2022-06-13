d71e73bb2afa4f373b91b6b7f384bcb0d52a91c3
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 17:40:00 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 1 Exerecício 3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string compositor = "Chico Buarque";
+            string genero = " MPB ";
+            string letra = " \n" + "  Meu tempo é curto" + " \n " +
+                           " O tempo dela sobra" + " \n " +
+                           " Meu cabelo é cinza" + " \n " +
+                           " O dela é cor de abóbora" + " \n " +
+                           " Temo que não dure muito a nossa novela " + " \n " +
+                           " Mas, eu sou tão feliz com ela" + " \n " +
+                           " Meu dia voa e ela não acorda" + " \n " +
+                           " Vou até a esquina, ela que ir pra Flórida" + " \n " +
+                           " Acho que nem sei direito o que é que ela fala" + " \n " +
+                           " Mas, não canso de contemplá-la" + " \n " +
+                           " Feito avarento conto os meus minutos" + " \n " +
+                           " Cada segundo que se esvai" + " \n " +
+                           " Cuidando dela que anda noutro mundo" + " \n " +
+                           " Ela que esbanja suas horas ao vento, ai" + " \n " +
+                           " Às vezes ela pinta a boca e saiFique a vontade, eu digo take on time" + " \n " +
+                           " Sinto que inda vou penar com essa pequenaMas, o blues já valeu à pena." + " \n ";
+
+            Console.WriteLine($"Compositor: {compositor}\nGênero: {genero}\nLetra: {letra} ");
+        }
+
+    }
+}
